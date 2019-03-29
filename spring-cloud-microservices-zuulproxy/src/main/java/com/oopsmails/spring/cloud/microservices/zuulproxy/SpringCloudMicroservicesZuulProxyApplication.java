4cbc0552df2b0a5c8561732e943f1bc34a245bc3
oopsmails <oopsmails@gmail.com>
2019-03-28 22:29:45 -0400

    20190328: zuul debugging

--- spring-cloud-microservices-zuulproxy/src/main/java/com/oopsmails/spring/cloud/microservices/zuulproxy/SpringCloudMicroservicesZuulProxyApplication.java
+++ spring-cloud-microservices-zuulproxy/src/main/java/com/oopsmails/spring/cloud/microservices/zuulproxy/SpringCloudMicroservicesZuulProxyApplication.java
@@ -1,9 +1,11 @@
 package com.oopsmails.spring.cloud.microservices.zuulproxy;
 
+import com.oopsmails.spring.cloud.microservices.zuulproxy.filter.HttpLoggingFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -14,5 +16,16 @@ public class SpringCloudMicroservicesZuulProxyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudMicroservicesZuulProxyApplication.class, args);
 	}
-	
+
+	@Bean
+	public FilterRegistrationBean<HttpLoggingFilter> httpLoggingFilter() {
+		HttpLoggingFilter httpLoggingFilter = new HttpLoggingFilter();
+		FilterRegistrationBean<HttpLoggingFilter> registrationBean = new FilterRegistrationBean<>();
+
+		registrationBean.setFilter(httpLoggingFilter);
+		registrationBean.addUrlPatterns("/*", "/uaa/*");
+		registrationBean.setName("httpLoggingFilter");
+		return registrationBean;
+	}
+
 }
